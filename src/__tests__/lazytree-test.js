93e1dce31dcc20473c0fe1956bdb27e10d5def3d
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-11-23 16:03:24 -0500

    mocha/chai test harness. But hard to transpile JSX with mocha

--- src/__tests__/lazytree-test.js
+++
@@ -1,8 +0,0 @@
-jest.dontMock('../lazytree.jsx');
-
-describe('lazytree', function() {
-    it('does stuff', function() {
-        var LazyTree = require('../lazytree.jsx');
-        expect(LazyTree.getInitialState.spacerHeights).toBe([0,0]);
-    });
-});
