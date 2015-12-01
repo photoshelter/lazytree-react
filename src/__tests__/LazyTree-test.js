b124bb4f05a69f3e7f1ee839ce7e8cc0f2dfdfe5
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2015-12-01 18:38:36 -0500

    First jest test passing.

--- src/__tests__/LazyTree-test.js
+++ src/__tests__/LazyTree-test.js
@@ -1,11 +1,11 @@
 jest.dontMock('../lazytree.jsx');
 jest.dontMock('../lazynode.jsx');
 
-var LazyTree = require('../lazytree.jsx');
+//var LazyTree = require('../lazytree.jsx');
 
 var React = require('react');
 var TestUtils = require('react-addons-test-utils');
-//var expect = require('chai').expect;
+var expect = require('chai').expect;
 
 describe('lazytree', function() {
     it('does stuff', function() {
@@ -16,6 +16,7 @@ describe('lazytree', function() {
                 nodeHeight = {30}
             ></LazyTree>
         );
-        expect(LazyTree.getInitialState().spacerHeights).to.equal([0,0]);
+        expect(tree.getInitialState().spacerHeights[0]).to.equal(0);
+        expect(tree.getInitialState().spacerHeights[1]).to.equal(0);
     });
 });
